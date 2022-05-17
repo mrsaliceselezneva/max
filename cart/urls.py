b02eda90e679b579d6d848eeb3cfcaa4e5e53f2f
mrsaliceselezneva <poluvesov.artyom@gmail.com>
2022-05-17 23:20:53 +0400

    max

---
+++ cart/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from cart import views
+
+app_name = 'cart'
+
+urlpatterns = [
+    path('order/', views.view_order, name='order'),
+    path('add/', views.add_to_cart, name='add_to_cart',),
+    path('delete/', views.delete_cart, name='delete_cart',),
+    path('', views.view_cart, name='cart',),
+]
